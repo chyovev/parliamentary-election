470340d10d40e993a6a82d73c800190d29498fa6
Christo Yovev <yovev@xroads-bg.com>
2021-03-21 22:55:04 +0100

    Initialize (add jQuery & Lou Multi-Select plugin)

---
+++ public/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="bg">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <title>Разпределяне на мандати при парламентарни избори в Република България</title>
+    <meta property="og:title" content="Разпределяне на мандати при парламентарни избори в Република България" />
+    <meta name="keywods" content="избори, мандати, събрание, гласове, партии, разпределяне" />
+    <link type="text/css" rel="stylesheet" href="css/style.css" />
+    <script src="js/script.js"></script>
+</head>
+<body>
+    <div class="body-wrapper">
+        <header>
+            <a href="#" class="coat"><img src="img/coat.png" alt="Герб на Република България" title="Начало" /></a>
+            <div class="title">Разпределяне на мандати<br /> при парламентарни избори<br /> в Република България</div>
+        </header>
+        <main>
+            
+        </main>
+        <footer>Разпределяне на мандати при парламентарни избори в Република България</footer>
+    </div>
+</body>
+</html>
